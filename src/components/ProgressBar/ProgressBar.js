be34182e653f158bbfb43d4ef78140345def6a03
Brian Schlenker <bschlenk@umich.edu>
2021-03-28 19:48:14 -0400

    Implement progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,47 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper
+      className={size}
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <Bar width={value} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  border-radius: 4px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
+  height: 8px;
+  background: ${COLORS.transparentGray15};
+
+  &.medium {
+    height: 12px;
+  }
+
+  &.large {
+    border-radius: 8px;
+    height: 24px;
+    padding: 4px;
+  }
+`;
+
+const Bar = styled.div`
+  background: ${COLORS.primary};
+  height: 100%;
+  width: 100%;
+  clip-path: inset(0 ${(p) => 100 - p.width}% 0 0);
+
+  ${Wrapper}.large & {
+    border-radius: 4px;
+  }
+`;
 
 export default ProgressBar;
